e409aa2a1124392fb87ecaa5a6a1748e7290fe8d
gavin-d26 <51263285+gavin-d26@users.noreply.github.com>
2025-03-16 15:19:41 -0700

    added comments in generation pipeline

--- run_generation_pipeline.sh
+++ run_generation_pipeline.sh
@@ -16,9 +16,21 @@ OUTPUT_CSV="${GENERATION_OUTPUTS_DIR}/${GENERATION_DATASET}.jsonl"
 # Config files
 CONFIG_FILE="${INFERENCE_PIPELINES_DIR}/inference_configs.yaml"
 
+# convert the base label .csv file to a hf dataset for the inference_engine
 python ${INFERENCE_PIPELINES_DIR}/dataset_conversion.py --input_path ${INPUT_SPLIT_CSV} --output_path ${DATASET_PATH}
+
+# run the inference engine on the hf dataset and add a new column with the llm responses
 python -m inference_pipelines.inference_engine --input-dataset-path ${DATASET_PATH} --result-dataset-path ${DATASET_PATH} --section data_generation --config ${CONFIG_FILE}
+
+# convert the hf dataset into a .csv or .jsonl
 python ${INFERENCE_PIPELINES_DIR}/dataset_conversion.py --input_path ${DATASET_PATH} --output_path ${OUTPUT_CSV}
+
+# for each row in the input dataset, the llm will generate multiple sample outputs. These need to be cleaned and exploded.
 python ${INFERENCE_PIPELINES_DIR}/collect_generations.py --input_path ${OUTPUT_CSV} --output_path ${OUTPUT_CSV} --column_to_explode "generated_text"
+
+# the llm generated reponse is a text sentence with xml tags, for ex : <emotional_fallacy>this is a bad script</emotional_fallacy>.
+# This raw llm outputs need to be converted to span labels ie. [[0, 23, "emotional_fallacy"]]
 python ${INFERENCE_PIPELINES_DIR}/get_spans.py --input_path ${OUTPUT_CSV} --output_path ${OUTPUT_CSV} --config ${CONFIG_FILE} --section convert_llm_response_to_span_annotations_for_generation
+
+# The previous script will clean all xml tags from the llm outputs. for easier vizualization of the text spans we create a new column with the xml tags added back into the text sentence.
 python ${INFERENCE_PIPELINES_DIR}/spans_to_xml_tags.py --input_path ${OUTPUT_CSV} --output_path ${OUTPUT_CSV} --text_column "generated_text" --span_annotation_col label --xml_column "vizualize_spans"
