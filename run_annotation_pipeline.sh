387ac91edce854babfcbaed0b849c76a0428e21b
gavin-d26 <51263285+gavin-d26@users.noreply.github.com>
2025-03-16 17:15:46 -0700

    Update dataset conversion and evaluation script parameters in run_annotation_pipeline.sh to improve consistency and accuracy in processing annotations.

--- run_annotation_pipeline.sh
+++ run_annotation_pipeline.sh
@@ -16,7 +16,7 @@ CONFIG_FILE="${INFERENCE_PIPELINES_DIR}/inference_configs.yaml"
 
 # Step 1: Convert input dataset to desired format
 # Converts the input JSONL file to a processed dataset, keeping only the "text" column
-python -m inference_pipelines.dataset_conversion --input_path ${INPUT_FILE} --output_path ${DATASET_PATH} --columns_to_keep "text"
+python -m inference_pipelines.dataset_conversion --input_path ${INPUT_FILE} --output_path ${DATASET_PATH}
 
 # Step 2: Run LLM inference for data annotation
 # Uses the configuration specified in inference_configs.yaml to run inference using the specified LLM
@@ -31,4 +31,4 @@ python -m inference_pipelines.get_spans --input_path ${OUTPUT_FILE} --output_pat
 
 # Step 5: Calculate F1 scores using IoU
 # between annotations on a sample of 180 instances, excluding "non-fallacy" labels
-python scripts/evaluate.py --input_path ${OUTPUT_FILE} --gold_span_col "final_annotations" --predicted_span_col "generated_annotations"
+python scripts/evaluate.py --input_path ${OUTPUT_FILE} --gold_span_col "final_annotation" --predicted_span_col "generated_annotations"
